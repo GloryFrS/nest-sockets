c2729cea58ac9a847f29dde62debcf7306d543ac
Vladimir Peshkov <peshkov@magora-systems.com>
2021-02-28 20:33:30 +0700

    add parser

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    EventsModule,
+    HttpModule.registerAsync({
+      useFactory: () => ({
+        baseURL: 'https://apprin.sgugit.ru',
+        timeout: 5000,
+        responseType: 'stream',
+        headers: {
+          Cookie:
+            'JSESSIONID=881b2ab9dadca0821aef6aec26d6; ts=l0nUz7jm3irIL1fKZ9n4yJ4Y9!akTx1NfaG15GWm;',
+        },
+        maxRedirects: 5,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AxiosResponse } from 'axios';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private httpService: HttpService) {}
+
+  async goParse() {
+    for (let index = 118635; index > 118435; index--) {
+      await this.getFile(index);
+    }
+
+    console.log('end');
+
     return 'Hello World!';
   }
+
+  timeoutTT(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async getFile(index) {
+    await this.timeoutTT(2000);
+
+    const fileName = `${index}.doc`;
+    const url = `/get_file?razdel=teacher_materials&id=${index}&name=%D0%BA%D1%80.doc`;
+    return this.httpService
+      .get(url)
+      .toPromise()
+      .then((response) => {
+        // response.data.on('data', data => console.log(data.toString().includes('нет валидной сессии')))
+        response.data.pipe(fs.createWriteStream('../files/' + fileName));
+      });
+  }
+
   getAll(): string {
     return 'get all';
   }
